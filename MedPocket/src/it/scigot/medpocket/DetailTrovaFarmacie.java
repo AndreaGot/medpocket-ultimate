eed78c03f3d73e8ffd770dd655574a88cd7cc981
sbongiz <a.sciarretta.tn@gmail.com>
2014-06-22 18:00:22 +0200

    lalalalal

--- MedPocket/src/it/scigot/medpocket/DetailTrovaFarmacie.java
+++ MedPocket/src/it/scigot/medpocket/DetailTrovaFarmacie.java
@@ -5,15 +5,38 @@ import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
 
 
 public class DetailTrovaFarmacie extends FragmentActivity {
 
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_detail_trova_farmacie);
-	}
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_trova_farmacie);
+
+        // Get a handle to the Map Fragment
+        GoogleMap map = ((MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+    }
+	
+	
+//	@Override
+//	protected void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_detail_trova_farmacie);
+//	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
