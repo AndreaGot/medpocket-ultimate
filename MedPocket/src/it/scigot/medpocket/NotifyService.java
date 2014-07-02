4c2c9371f8746ea36673606f247d6f8798f0cd1d
Andrea Gottardi <andreagot16@gmail.com>
2014-07-02 23:08:35 +0200

    Error fixed (class called on Notification)

--- MedPocket/src/it/scigot/medpocket/NotifyService.java
+++ MedPocket/src/it/scigot/medpocket/NotifyService.java
@@ -79,7 +79,7 @@ public class NotifyService extends Service {
         Notification notification = new Notification(icon, text, time);
  
         // The PendingIntent to launch our activity if the user selects this notification
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, Detail.class), 0);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, DetailPromemoria.class), 0);
  
         // Set the info for the views that show in the notification panel.
         notification.setLatestEventInfo(this, title, text, contentIntent);
